4b5dd8899656ae3a871947cef8c631b8b3699bcf
alejoescobar <alejoescobac@gmail.com>
2016-05-10 15:52:49 -0500

    Add counts data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+  require 'open-uri'
+
   def home
+    request = open("https://api.instagram.com/v1/users/self/?access_token=#{current_user.token}")
+    data = request.read
+    @user_data = JSON.parse(data)['data']
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  require 'open-uri'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :omniauthable,
          :omniauth_providers => [:instagram]
+
+  after_create :initial_counts
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -15,4 +18,12 @@ class User < ApplicationRecord
       # user.followed_by = auth.info.counts.followed_by
     end
   end
+
+  def initial_counts
+    request = open("https://api.instagram.com/v1/users/self/?access_token=#{self.token}")
+    data = request.read
+    user_data = JSON.parse(data)['data']
+    self.update(follows: user_data['counts']['follows'], followed_by: user_data['counts']['followed_by'])
+  end
+
 end
