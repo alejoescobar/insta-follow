b504bdac12b725d6aaba862ce24bc5a59de2feea
alejoescobar <alejoescobac@gmail.com>
2016-05-10 00:15:03 -0500

    Initial instagram omnioauth config

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         :omniauth_providers => [:instagram]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.data.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.data.username   # assuming the user model has a name
+      user.image = auth.data.profile_picture # assuming the user model has an image
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -241,7 +241,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :instagram, '92d62907daa7499baf099bf0153e2756', '383f1716920e4da7b2bb8d38b5148285',
+  scope: 'follower_list', callback_url: "http://localhost:3000"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
